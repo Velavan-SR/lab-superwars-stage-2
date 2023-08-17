d1142ddab512f8529e6ec75f88fa75b9e56f02f9
Velavan Sundharamoorthy <velavan.sundharamoorth@kalvium.community>
2023-08-17 11:28:33 +0530

    mission accomplished

--- src/app.js
+++ src/app.js
@@ -21,19 +21,36 @@ const PLAYERS = [
     "Slingo"
 ];
 
-// initialize players with image and strength
-const initPlayers = (players) => {
-    let detailedPlayers = [];
-    // Create players using for loop
-    // Type your code here
-
-    return detailedPlayers;
-}
-
 // getting random strength
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.floor(Math.random()*101)
+}
+
+function type(i){
+    if (i%2 === 0){
+        return "hero"
+    }else{
+        return "villain"
+    }
+}
+
+
+// initialize players with image and strength
+const initPlayers = (players) => {
+    let detailedPlayers = [];
+
+    // Create players using for loop
+    // Type your code here
+    for (let i = 0; i<players.length; i++){
+        detailedPlayers.push({name: players[i],
+            img: "images/super-"+(i+1)+".png",
+            strength: getRandomStrength(),
+            type: type(i)})
+    }
+    return detailedPlayers;
+
 }
 
 const buildPlayers = (players, type) => {
@@ -42,6 +59,12 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+
+    for (let i=0; i<players.length; i++){
+        if (players[i].type==type){
+            fragment += "<div class='player'><img src="+players[i].img+"> <div class='name'>"+players[i].name+"</div>"+"<div class='strength'>"+players[i].strength+"</div></div>"
+        }
+    }
 
     return fragment;
 }
@@ -56,3 +79,5 @@ const viewPlayers = (players) => {
 window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
 }
+
+console.log(initPlayers(PLAYERS))
